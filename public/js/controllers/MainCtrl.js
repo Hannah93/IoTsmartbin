2b9fb160ab240b5f709a98a99d202143c1408150
Hannah93 <Hannelore.hauquier@hotmail.com>
2016-01-01 15:59:48 +0100

    added timeout to update data automatically

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,10 +1,31 @@
-angular.module('MainCtrl', []).controller('MainController', ["$rootScope", "$http", "$scope", function ($rootScope, $http, $scope)  {
+angular.module('MainCtrl', []).controller('MainController', ["$rootScope", "$http", "$scope","$timeout", function ($rootScope, $http, $scope, $timeout)  {
 
   $http.get('/smartbinData')
       .success(function(data) {
           $scope.smartbins = data; //Expose the user data to your angular scope
           console.log(data);
               });
+
+  // Function to get the data
+    $scope.getData = function(){
+      $http.get('/smartbinData')
+          .success(function(data) {
+              $scope.smartbins = data; //Expose the user data to your angular scope
+              console.log(data);
+                  });
+    };
+
+    // Function to replicate setInterval using $timeout service.
+    $scope.intervalFunction = function(){
+      $timeout(function() {
+        $scope.getData();
+        $scope.intervalFunction();
+      }, 10000)
+    };
+
+    // Kick off the interval
+    $scope.intervalFunction();
+
 
   document.body.style.background = "#CEF6F5 url('../img/Achtergrond.png') no-repeat right top"
 }]);
