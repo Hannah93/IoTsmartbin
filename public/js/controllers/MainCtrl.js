774c328c4f83cd521240b96b31207b06e02ff764
Hannah93 <Hannelore.hauquier@hotmail.com>
2016-01-01 14:52:50 +0100

    display data on home page

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,9 +1,10 @@
-angular.module('MainCtrl', []).controller('MainController', ["$rootScope", "$http", function ($rootScope, $http)  {
+angular.module('MainCtrl', []).controller('MainController', ["$rootScope", "$http", "$scope", function ($rootScope, $http, $scope)  {
 
   $http.get('/smartbinData')
       .success(function(data) {
           $scope.smartbins = data; //Expose the user data to your angular scope
-      });
+          console.log(data);
+              });
 
   document.body.style.background = "#CEF6F5 url('../img/Achtergrond.png') no-repeat right top"
 }]);
